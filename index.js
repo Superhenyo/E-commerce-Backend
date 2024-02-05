14cdedafa4d1559b3dc5e3d2afc73d2e22af72a0
atinanemilb <atinanemilb@gmail.com>
2024-02-05 11:11:30 +0800

    adjust env

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const multer = require('multer');
+const dotenv = require('dotenv')
 
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product")
+
+dotenv.config({ path: 'C:/Users/chris/Desktop/Porfolio/Web Development/Bootcamp_capstone2_Render/Back End/.env.local' });
+
+console.log('After dotenv.config() - Backend_Mongoose:', process.env);
+console.log('After dotenv.config() - Backend_Mongoose:', process.env.Backend_Mongoose);
 
 const port = 4002;
 const app = express();
@@ -14,10 +19,12 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(cors());
 
+
 mongoose.connect(process.env.Backend_Mongoose, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
 
 mongoose.connection.once('open', () => console.log("Now connected to MongoDB Atlas."))
 
